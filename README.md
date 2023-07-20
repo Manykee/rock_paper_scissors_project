8dc71e689bc5a49b3f92fc80f5bfd96edb0059f0
Manykee <124197328+Manykee@users.noreply.github.com>
2023-07-20 22:19:44 +0300

    Created the index.html file and the script.js file, and linked them. Made the pseudocode

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rock_paper_scissors_project
+A project made from scratch for "The Odin Project"
---
+++ script.js
@@ -0,0 +1,6 @@
+// Get computer choice function
+// Write a function to play a single round of the game
+// Get the player's selection, case-insesitive
+// Get computer's selection
+// Return a string with "You loose!" or "You win!"
+// Write a function to loop the game for 5 times
