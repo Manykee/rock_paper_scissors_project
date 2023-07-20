795e61a69a3e6cd9c2370d3c094280a19514e622
Manykee <124197328+Manykee@users.noreply.github.com>
2023-07-20 22:44:25 +0300

    Created the function for getting the computer's random choice

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // Get computer choice function
+
+function getComputerChoice() {
+    let choice = ["Rock", "Paper", "Scissor"];
+    let number = Math.floor(Math.random() * choice.length);
+    return choice[number];
+}
 // Write a function to play a single round of the game
 // Get the player's selection, case-insesitive
 // Get computer's selection
